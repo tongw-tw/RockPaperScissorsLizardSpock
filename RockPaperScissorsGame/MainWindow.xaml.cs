ec5ababfeb4bf65664ff6eb1abbe21e25921b0d2
Tong Wang <tong_w@outlook.com>
2020-10-28 15:36:39 -0400

    upload files

---
+++ RockPaperScissorsGame/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows;
+
+namespace RockPaperScissorsGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM();
+        public int value;
+        public string src;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+            value = vm.CompChoice();
+            HideChoiceImg();
+        }
+        private void rockBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.UserChoice(0);
+            vm.WinOrLose(value, 0);
+            ShowChoiceImg(value, 0);
+            value = vm.CompChoice();
+        }
+        private void lizardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.UserChoice(1);
+            vm.WinOrLose(value, 1);
+            ShowChoiceImg(value, 1);
+            value = vm.CompChoice();
+        }
+        private void spockBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.UserChoice(2);
+            vm.WinOrLose(value, 2);
+            ShowChoiceImg(value, 2);
+            value = vm.CompChoice();
+        }
+        private void scissorsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.UserChoice(3);
+            vm.WinOrLose(value, 3);
+            ShowChoiceImg(value, 3);
+            value = vm.CompChoice();
+        }
+        private void paperBtn_Click(object sender, RoutedEventArgs e)
+        {
+            vm.UserChoice(4);
+            vm.WinOrLose(value, 4);
+            ShowChoiceImg(value, 4);
+            value = vm.CompChoice();
+        }
+        private void ShowChoiceImg(int compNum, int playerNum)
+        {
+            HideChoiceImg();
+            if (playerNum == 0)
+                playerRock.Visibility = Visibility.Visible;
+            else if (playerNum == 1)
+                playerLizard.Visibility = Visibility.Visible;
+            else if (playerNum == 2)
+                playerSpock.Visibility = Visibility.Visible;
+            else if (playerNum == 3)
+                playerScissors.Visibility = Visibility.Visible;
+            else
+                playerPaper.Visibility = Visibility.Visible;
+            if (compNum == 0)
+                compRock.Visibility = Visibility.Visible;
+            else if (compNum == 1)
+                compLizard.Visibility = Visibility.Visible;
+            else if (compNum == 2)
+                compSpock.Visibility = Visibility.Visible;
+            else if (compNum == 3)
+                compScissors.Visibility = Visibility.Visible;
+            else
+                compPaper.Visibility = Visibility.Visible;
+        }
+        private void HideChoiceImg()
+        {
+            compRock.Visibility = Visibility.Hidden;
+            compLizard.Visibility = Visibility.Hidden;
+            compSpock.Visibility = Visibility.Hidden;
+            compScissors.Visibility = Visibility.Hidden;
+            compPaper.Visibility = Visibility.Hidden;
+            playerRock.Visibility = Visibility.Hidden;
+            playerLizard.Visibility = Visibility.Hidden;
+            playerSpock.Visibility = Visibility.Hidden;
+            playerScissors.Visibility = Visibility.Hidden;
+            playerPaper.Visibility = Visibility.Hidden;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ResetScore();
+            HideChoiceImg();
+        }
+    }
+}
---
+++ RockPaperScissorsGame/VM.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RockPaperScissorsGame
+{
+    public class VM : INotifyPropertyChanged
+    {
+        private readonly Random random = new Random();
+        private String compWin = "0";
+        public String CompWin
+        {
+            get { return compWin; }
+            set { compWin = value; onChange(); }
+        }
+        private int compWinNum = 0;
+        private String playerWin = "0";
+        public String PlayerWin
+        {
+            get { return playerWin; }
+            set { playerWin = value; onChange(); }
+        }
+        private int playerWinNum = 0;
+        public void ResetScore()
+        {
+            compWinNum = 0;
+            CompWin = compWinNum.ToString();
+            playerWinNum = 0;
+            PlayerWin = playerWinNum.ToString();
+            Message = null;
+            onChange();
+        }
+        public int CompChoice()
+        {
+            int value = random.Next(0, 5);
+            return value;
+        }
+        public int UserChoice(int x)
+        {
+            return x;
+        }
+        private String message;
+        public String Message
+        {
+            get { return message; }
+            set { message = value; onChange(); }
+        }
+        public void WinOrLose(int CompNum, int UserNum)
+        {
+            int diff = (CompNum - UserNum);
+            if (diff < 0) diff += 5;
+            if (diff == 2 | diff == 4)
+            {
+                Message = "Computer wins!";
+                compWinNum++;
+                CompWin = compWinNum.ToString();
+                onChange();
+            }
+            else if (diff == 1 | diff == 3)
+            {
+                Message = "User wins!";
+                playerWinNum++;
+                PlayerWin = playerWinNum.ToString();
+                onChange();
+            }
+            else
+            {
+                Message = "Computer and User tie!";
+                onChange();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onChange([CallerMemberName]string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
